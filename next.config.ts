a436e9717c9ef49c2fc24cefe26b1907ff507027
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-07 14:02:21 -0600

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: process.env.NEXT_OUTPUT === "export" ? "export" : undefined,
+	env: {
+		NEXT_TITLE: 'VICTIVA',
+	},
+	images: {
+		unoptimized: true,
+		domains: [], // Add domains for external images if needed
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
