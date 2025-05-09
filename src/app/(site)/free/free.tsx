1e76df81d13b628691dfdd89ce35f64319e9117d
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-09 13:06:37 -0600

    added new accordions

--- src/app/(site)/free/free.tsx
+++ src/app/(site)/free/free.tsx
@@ -1,9 +1,11 @@
+import {Grid} from '@mui/material';
+
 export default function Free() {
 	return (
 		<div id="free" className={'titlePageFree'}>
-			<h1>
-				Бесплатно
-			</h1>
+			<Grid container columns={12}>
+
+			</Grid>
 		</div>
 	)
 }
