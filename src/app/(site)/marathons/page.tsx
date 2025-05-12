825cb19fbca4428ade7adc39ebab11bf62dbe2dd
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-12 13:51:32 -0600

    added MarathonsPage page

--- src/app/(site)/marathons/page.tsx
+++ src/app/(site)/marathons/page.tsx
@@ -1,7 +1,35 @@
+import {Box, Grid, Paper} from '@mui/material';
+import homeImageCurs from '@/public/homeImage.png';
+import NewPrice from '@/src/app/(components)/new-price/new-price';
+import StripeButton from '@/src/app/(components)/button/stripeButton';
+import imageExcelent from '@/src/app/(site)/curs/images/excelent.png';
+import Carousel from 'react-material-ui-carousel';
+
 export default function MarathonsPage() {
 	return (
-		<div>
-			MarathonsPage
-		</div>
+		<Grid id="curs" className={'titlePageCurs'}
+		      style={{
+			      height: 'calc(100vh - 64px - 3rem)',
+			      display: 'flex',
+			      flexDirection: 'column',
+			      alignItems: 'center',
+			      justifyContent: 'center',
+			      textAlign: 'center'
+		      }}
+		>
+			<h1>
+				<div style={{ marginBottom: '10px' }}>МАРАФОНЫ</div>
+			</h1>
+			<img src={homeImageCurs.src} className="mx-auto"
+			     style={{
+				     margin: 20,
+				     width: '60%',
+			     }} alt="homeImage"
+			/>
+
+			<NewPrice oldParam={{value: 299, color: 'white'} } newParam={{value: 49, color: 'red'}} />
+			<div style={{ margin: '1rem' }}>ГОВОРИ КАК АМЕРИКАНЦЫ ЗА 8 НЕДЕЛЬ</div>
+			<StripeButton value={4900} />
+		</Grid>
 	)
 }
