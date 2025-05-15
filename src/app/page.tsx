f8556690378e3e7cdfb08c6d77df5c3dad73885b
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-15 17:43:53 -0600

    added Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Connect from "./(site)/connect/page";
 import Curs from "./(site)/curs/page";
 import Free from "./(site)/free/free";
 import HomePage from "./(site)/home/page";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
      <div>
+        <Analytics />
         <HomePage/>
         <Free />
         <Curs/>
