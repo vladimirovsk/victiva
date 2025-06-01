3fc48544c91d36b9e569dc822a269267443f42a4
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-31 18:53:55 -0600

    modify curs page

--- src/app/(components)/message-svg/message-svg.tsx
+++ src/app/(components)/message-svg/message-svg.tsx
@@ -4,17 +4,17 @@ export default function MessageSvg() {
   return (
       <div id="comp-lzj1loyg" className="comp-lzj1loyg wixui-vector-image">
         <div data-testid="svgRoot-comp-lzj1loyg" className="AKxYR5 VZYmYf comp-lzj1loyg">
-          <svg preserveAspectRatio="none" data-bbox="20 20 200 160" viewBox="20 20 200 160" height="400" width="500"
+          <svg preserveAspectRatio="none" data-bbox="20 20 200 180" viewBox="20 20 200 160" height="400" width="500"
                xmlns="http://www.w3.org/2000/svg" data-type="shape" role="img" aria-label="Рамка с текстом о Калласасе">
             <g>
               <path d="M220 20H20v118.474h167.041l16.889 10.637 1.41.889-.346-1.624-2.101-9.902H220V20z"></path>
             </g>
-            <text x="40" y="50" fill="black" fontSize="11" fontFamily="Montserrat">
-              <tspan x="40" dy="0">Что!? Вы еще не решились?</tspan>
-              <tspan x="40" dy="20">Вот, посмотрите что я сделал</tspan>
-              <tspan x="40" dy="20">когда ко мне начал говорить</tspan>
-              <tspan x="40" dy="20">Калласас со своим русским</tspan>
-              <tspan x="40" dy="20">акцентом</tspan>
+            <text x="40" y="50" fill="black" fontSize="10" fontFamily="Montserrat">
+              <tspan x="30" dy="0">Что!? Вы еще не решились?</tspan>
+              <tspan x="30" dy="20">Вот, посмотрите что я сделал</tspan>
+              <tspan x="30" dy="20">когда ко мне начал говорить</tspan>
+              <tspan x="30" dy="20">Калласас со своим русским</tspan>
+              <tspan x="30" dy="20">акцентом</tspan>
             </text>
           </svg>
         </div>
